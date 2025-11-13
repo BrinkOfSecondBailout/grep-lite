5e3dc6fb01626dd583646d9a4e91abed469210ce
Danny Nguyen <nguyenduongpa@gmail.com>
2025-11-12 18:06:39 -0800

    finished without cli

--- src/main.rs
+++ src/main.rs
@@ -2,50 +2,37 @@ use clap::{App, Arg};
 use regex::Regex;
 use std::{
     fs::File,
-    io,
-    io::{BufReader, prelude::*},
+    io::{self, BufReader, prelude::*, stdin},
 };
 
 fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
-    println!("Searching for {}...\n", re);
+    println!("Searching for '{}'...", re);
+    let mut found = false;
     for line_ in reader.lines() {
         let line = line_.unwrap();
         match re.find(&line) {
             Some(_) => {
-                println!("{}", line)
-            }
+                found = true;
+                println!("{}", line);
+            },
             None => (),
         }
+    }
+    if !found {
+        println!("No match for {}", re);
     }
 }
 
 fn main() {
-    let args = App::new("grep-lite")
-        .arg(
-            Arg::with_name("pattern")
-                .help("Pattern to search for")
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("input")
-                .help("File to search")
-                .takes_value(true)
-                .required(false),
-        )
+    let args = App::new("Grep-Lite")
+        .arg(Arg::with_name("pattern").help("Pattern to search for").takes_value(true).required(true))
+        .arg(Arg::with_name("input").help("Path/filename of text").takes_value(true).required(true))
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
+    let input = args.value_of("input").unwrap();
     let re = Regex::new(pattern).unwrap();
-    let input = args.value_of("input").unwrap_or("-");
-
-    if input == "-" {
-        let stdin = io::stdin();
-        let reader = stdin.lock();
-        process_lines(reader, re);
-    } else {
-        let f = File::open(input).unwrap();
-        let reader = BufReader::new(f);
-        process_lines(reader, re);
-    }
+    let file = File::open(input).unwrap();
+    let reader = BufReader::new(file);
+    process_lines(reader, re);
 }
